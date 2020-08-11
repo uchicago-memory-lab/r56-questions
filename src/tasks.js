77f11d90b72147e6fe2a1e8764cb0f8bbcf499e6
SoyBison <coen@uchicago.edu>
2020-08-11 18:04:43 -0500

    STRESS

--- src/tasks.js
+++ src/tasks.js
@@ -37,7 +37,7 @@ function dataBlock(data){
 
 function testItemFinder(jsPsychData){
     /** Input should be in the form of jsPsych.data.get(), so this works with .filterCustom() */
-    console.log('jsPsychData')
+    console.log(jsPsychData)
     if ('data' in jsPsychData){
         console.log('trigger0')
         if ('stored' in jsPsychData){
