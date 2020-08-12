6a47b4b372753329f68bdbdae4b3f65763b8c072
SoyBison <coen@uchicago.edu>
2020-08-12 16:32:10 -0500

    added emwordstim practice :sparkles:

--- write_data.php
+++ write_data.php
@@ -3,7 +3,8 @@
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 
-
-try {
+try{
     $conn = pg_connect(getenv("DATABASE_URL"));
-}
+} catch(Exception $e) {
+    echo '{"success": false, "message": ' . $e->getMessage() . '}';
+  }
