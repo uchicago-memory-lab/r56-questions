1ff1bfb9160cf4fc423f4824d2f0cd430268f80c
SoyBison <coen@uchicago.edu>
2020-08-17 13:23:57 -0500

    datatesting

--- src/tasks.js
+++ src/tasks.js
@@ -19,6 +19,7 @@ function saveData() {
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function() {
         if(xhr.status === 200){
+            console.log(xhr.responseText)
             var response = JSON.parse(xhr.responseText);
             console.log(response.success);
         }
