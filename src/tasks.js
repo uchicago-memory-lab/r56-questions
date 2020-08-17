ab38b8a8533f76ece086bc6e283e720c141f0e32
SoyBison <coen@uchicago.edu>
2020-08-17 11:20:45 -0500

    datatesting

--- src/tasks.js
+++ src/tasks.js
@@ -12,7 +12,7 @@ async function getData(url) {
 
 function saveCSV(name, data){
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file described above.
+    xhr.open('POST', 'writecsv.php'); // 'write_data.php' is the path to the php file described above.
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({filename: name, filedata: data}));
   }
