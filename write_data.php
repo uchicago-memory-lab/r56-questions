3a2741c22f75d24a976089ec54a04ea1e027aff7
SoyBison <coen@uchicago.edu>
2020-08-31 17:34:44 -0500

    Some preparation while I was planning :art:

--- write_data.php
+++ write_data.php
@@ -5,11 +5,11 @@ $db = parse_url(getenv("DATABASE_URL"));
 // Heroku handles the connstring part of this. 
 // Presumably our credentials are safe so we needn't take any safety measures here?
 
-
-// heck
 $data_array = json_decode(file_get_contents("php://input"), true);
 
-
+$temp_username = 'YeetBird123';
+// This will be the row name until I get the qualtrics side of things built. 
+// I'll delete that line later.
 
 try{
     $pdo = new PDO("pgsql:" . sprintf(
@@ -20,6 +20,7 @@ try{
         $db["pass"],
         ltrim($db["path"], "/")
     ));
+    
 
 } catch(Exception $e) {
     echo '{"success": false, "message": ' . $e->getMessage() . '}';
