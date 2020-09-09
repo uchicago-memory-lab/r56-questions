d526834e7a4f3e85b001382307bfcf2c323b31dd
SoyBison <coen@uchicago.edu>
2020-09-09 16:03:10 -0500

    accidentally yeeted a semicolon

--- stopwatch.php
+++ stopwatch.php
@@ -2,16 +2,8 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 
-$data_array = json_decode(file_get_contents("php://input")
+$data_array = json_decode(file_get_contents("php://input"));
 
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
 
 try{
     $pdo = new PDO("pgsql:" . sprintf(
@@ -26,8 +18,8 @@ try{
     $time = $data_array['time'];
     $pid = $data_array['pid'];
 
-    $pdo->query("INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)")
-    echo "INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)"
+    $pdo->query("INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)");
+    echo "INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)";
 } catch(\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
   }
