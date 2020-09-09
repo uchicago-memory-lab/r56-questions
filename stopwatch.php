2fb4ca9b3c630c4138cc8da9e962d0814048825e
SoyBison <coen@uchicago.edu>
2020-09-09 15:47:14 -0500

    debug code still

--- stopwatch.php
+++ stopwatch.php
@@ -27,7 +27,7 @@ try{
     $pid = $data_array['pid'];
 
     $pdo->query("INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)")
-    console_log("INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)")
+    echo "INSERT INTO time_elapsed (pid, time_elapsed) VALUES ('$pid', $time)"
 } catch(\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
   }
